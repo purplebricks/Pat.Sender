78ea46eb57000995760252258b95414970715823
Rob White <rob.j.white@gmail.com>
2018-08-07 17:42:47 +0100

    Add Xml Docs

--- Pat.Sender.NetCoreLog/PatSenderNetCoreLogAdapter.cs
+++ Pat.Sender.NetCoreLog/PatSenderNetCoreLogAdapter.cs
@@ -3,6 +3,7 @@ using System;
 
 namespace Pat.Sender.NetCoreLog
 {
+    /// <inheritdoc />
     public class PatSenderNetCoreLogAdapter : IPatSenderLog
     {
         private readonly ILogger log;
@@ -11,16 +12,20 @@ namespace Pat.Sender.NetCoreLog
         {
             this.log = log;
         }
+        
+        /// <inheritdoc />
+        public void LogCritical(string message)
+            => log.LogCritical(message);
 
-        public void LogCritical(string message, params object[] arguments)
-            => log.LogCritical(message, arguments);
-
+        /// <inheritdoc />
         public void LogInformation(string message)
             => log.LogInformation(message);
 
+        /// <inheritdoc />
         public void LogWarning(string message)
             => log.LogWarning(message);
 
+        /// <inheritdoc />
         public void LogWarning(string message, Exception exception)
             => log.LogWarning(exception, message);
     }
--- Pat.Sender/IPatSenderLog.cs
+++ Pat.Sender/IPatSenderLog.cs
@@ -2,13 +2,38 @@
 
 namespace Pat.Sender
 {
-    // Remove this when net451 support is dropped, using Microsoft.Extensions.Logging.Abstractions types instead.
-    // Also then remove the package Pat.Sender.NetCoreLog and *consider* removing Pat.Sender.Log4Net
+    /// <summary>
+    /// Represents a type used to perform logging.
+    /// </summary>
+    /// <remarks>
+    /// Remove this when net451 support is dropped, using Microsoft.Extensions.Logging.Abstractions types instead.
+    /// Also then remove the package Pat.Sender.NetCoreLog and *consider* removing Pat.Sender.Log4Net
+    /// </remarks>
     public interface IPatSenderLog
     {
+        /// <summary>
+        /// Writes an informational message to the log.
+        /// </summary>
+        /// <param name="message">The log entry</param>
         void LogInformation(string message);
+
+        /// <summary>
+        /// Writes a warning message to the log.
+        /// </summary>
+        /// <param name="message">The log entry</param>
         void LogWarning(string message);
+
+        /// <summary>
+        /// Writes a warning message to the log.
+        /// </summary>
+        /// <param name="message">The log entry</param>
+        /// <param name="exception">A related exception</param>
         void LogWarning(string message, Exception exception);
-        void LogCritical(string message, params object[] arguments);
+
+        /// <summary>
+        /// Writes a critical message to the log.
+        /// </summary>
+        /// <param name="message">The log entry</param>
+        void LogCritical(string message);
     }
 }
