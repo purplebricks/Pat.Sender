f24c0bb131947a11999b59c2fdcf17f8f397b14b
Paul Horton <Paul.Horton@purplebricks.com>
2022-12-06 01:03:39 +0000

    Creates a dependency resolution extension method

---
+++ Pat.Sender.NetCoreDependencyResolution/ServiceCollectionAddPatSender.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Pat.Sender.Correlation;
+using Pat.Sender.MessageGeneration;
+using Pat.Sender.NetCoreLog;
+
+namespace Pat.Sender.NetCoreDependencyResolution
+{
+    public static class ServiceCollectionAddPatSender
+    {
+        public static IServiceCollection AddPatSender(this IServiceCollection services, PatSenderSettings senderSettings)
+        {
+            services.AddSingleton(senderSettings);
+            services.AddSingleton<ICorrelationIdProvider, NewCorrelationIdProvider>();
+            services.AddPatSenderNetCoreLogAdapter();
+            services.AddSingleton<MessageProperties>();
+            services.AddTransient<IMessageSender, MessageSender>();
+            services.AddSingleton<IMessageGenerator, MessageGenerator>();
+
+            services.AddTransient<IMessagePublisher>(
+               p => new MessagePublisher(
+                   p.GetRequiredService<IMessageSender>(),
+                   p.GetRequiredService<IMessageGenerator>(),
+                   new MessageProperties(p.GetRequiredService<ICorrelationIdProvider>())));
+
+            services.AddSingleton(senderSettings);
+
+            return services;
+        }
+    }
+}
